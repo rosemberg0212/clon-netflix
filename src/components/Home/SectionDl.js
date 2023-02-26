afc5d65e652786bf8f8734cb6f1551e45c7b666b
Rous <rosemberc.g.02@gmail.com>
2023-02-26 18:36:39 -0500

    responsive home

--- src/components/Home/SectionDl.js
+++ src/components/Home/SectionDl.js
@@ -4,15 +4,32 @@ import img from '../../img/dwl.jpeg'
 const SectionDl = () => {
   return (
     <div className='SectionDl'>
-        <div className='img'>
-            <img src={img} alt='img'/>
-        </div>
-        <div className='info'>
+      {window.screen.width <= 760 ? (
+        <>
+          <div className='info'>
             <div className='sub'>
-                <h2>Descarga tus series para verlas offline.</h2>
-                <p>Guarda tu contenido favorito y tendrás siempre algo para ver.</p>
+              <h2>Descarga tus series para verlas offline.</h2>
+              <p>Guarda tu contenido favorito y tendrás siempre algo para ver.</p>
             </div>
-        </div>
+          </div>
+          <div className='img'>
+            <img src={img} alt='img' />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className='img'>
+            <img src={img} alt='img' />
+          </div>
+          <div className='info'>
+            <div className='sub'>
+              <h2>Descarga tus series para verlas offline.</h2>
+              <p>Guarda tu contenido favorito y tendrás siempre algo para ver.</p>
+            </div>
+          </div>
+        </>
+      )}
+
     </div>
   )
 }
--- src/components/Home/SectionNinos.js
+++ src/components/Home/SectionNinos.js
@@ -2,17 +2,37 @@ import React from 'react'
 import img from '../../img/niños.jpeg'
 
 const SectionNinos = () => {
+
+
     return (
         <div className='SectionNinos'>
-            <div className='img'>
-                <img src={img} alt='img' />
-            </div>
-            <div className='info'>
-                <div className='sub'>
-                    <h2>Crea perfiles para niños.</h2>
-                    <p>Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.</p>
-                </div>
-            </div>
+            {window.screen.width <= 760 ? (
+                <>
+                    <div className='info'>
+                        <div className='sub'>
+                            <h2>Crea perfiles para niños.</h2>
+                            <p>Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.</p>
+                        </div>
+                    </div>
+                    <div className='img'>
+                        <img src={img} alt='img' />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className='img'>
+                        <img src={img} alt='img' />
+                    </div>
+                    <div className='info'>
+                        <div className='sub'>
+                            <h2>Crea perfiles para niños.</h2>
+                            <p>Los niños vivirán aventuras con sus personajes favoritos en un espacio diseñado exclusivamente para ellos, sin costo con tu membresía.</p>
+                        </div>
+                    </div>
+                </>
+            )}
+
+
         </div>
     )
 }
